9fca9dd4010a9406fda35ab4481d0715ee522c67
Myszexx <michalr550@gmail.com>
2025-02-05 20:29:52 +0100

    Added React Buddy for WebStrom Support

---
+++ src/dev/previews.jsx
@@ -0,0 +1,27 @@
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
+import {PaletteTree} from './palette'
+import {Header} from "../components/Dashboard/Header.jsx";
+import {Profile} from "../pages/Profile.jsx";
+import {Registration} from "../pages/Registration.jsx";
+import {MainBoard} from "../pages/dashboard/MainBoard.jsx";
+
+const ComponentPreviews = () => {
+    return (
+        <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/Header">
+                <Header/>
+            </ComponentPreview>
+            <ComponentPreview path="/Profile">
+                <Profile/>
+            </ComponentPreview>
+            <ComponentPreview path="/Registration">
+                <Registration/>
+            </ComponentPreview>
+            <ComponentPreview path="/MainBoard">
+                <MainBoard/>
+            </ComponentPreview>
+        </Previews>
+    )
+}
+
+export default ComponentPreviews
